1cfacb6ab30a9acf20aaade256a78f590353da11
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-08-07 14:00:17 -0500

    Typing user and pwd

--- tag_ui/emr.py
+++ tag_ui/emr.py
@@ -1,4 +1,6 @@
 import json
+import rpa as r
+
 
 def get_credential_data(environment, **kwargs):
     credentials_file = kwargs.get('credentials_file', '../.creds/emr_cred.json')
@@ -6,11 +8,31 @@ def get_credential_data(environment, **kwargs):
         creds = json.load(json_file)
     return creds.get(environment)
 
-def login_to_admin(admin_url, username, password, robot):
-    pass
 
+def login_to_admin(admin_url, username, password):
+    r.url(admin_url)
+    #//Employee/[@id='4']
+
+    element = '//input[@id="id_username"]'
+    r.click(element)
+    r.type(element, username)
+
+    element = '//input[@id="id_password"]'
+    r.click(element)
+    r.type(element, password)
+    r.click('Log in.png')
 
 if __name__ == '__main__':
     env = 'staging'
     cred = get_credential_data(env)
-    print(cred)
+    #print(cred)
+    r.init()
+
+    ## LOGIN
+    admin_url = cred['base_url'] + cred['roles']['admin']['url']
+    username = cred['roles']['admin']['username']
+    password = cred['roles']['admin']['password']
+    login_to_admin(admin_url, username, password)
+
+    r.wait(30)
+    r.close()
