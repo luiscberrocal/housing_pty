2ac6054b094f4e732f9f2a17134b62eb576f9734
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-08-07 19:46:20 -0500

    Changing password

--- tag_ui/emr.py
+++ tag_ui/emr.py
@@ -11,19 +11,52 @@ def get_credential_data(environment, **kwargs):
 
 def login_to_admin(admin_url, username, password):
     r.url(admin_url)
-    #//Employee/[@id='4']
 
     element = '//input[@id="id_username"]'
-    r.click(element)
-    r.type(element, username)
+    if r.exist(element):
+        print('Login')
+        r.click(element)
+        r.type(element, username)
 
-    element = '//input[@id="id_password"]'
+        element = '//input[@id="id_password"]'
+        r.click(element)
+        r.type(element, password)
+        #print(element, password)
+
+        element = '//input[@type="submit"]'
+        r.click(element)
+    else:
+        print('Already logged in')
+
+def change_password(username, new_password):
+    #//*[@id="content-main"]/div[9]/table/tbody/tr/th/a
+    element = '//tr[@class="model-user"]/th/a'
     r.click(element)
-    r.type(element, password)
-    r.click('Log in.png')
+    #//Element[@attribute1="abc" and @attribute2="xyz" and text()="Data"]
+    #user_elem = f'//td[@class="field-name" and text()="{username}"]' #/preceding-sibling::th[@class="field-id"]/a'
+    user_elem = f'//td[text()="{username}"]/preceding-sibling::th[@class="field-id"]/a'
+    r.click(user_elem)
+    pwd_form_elem = '//a[@href="../password/"]'
+    r.click(pwd_form_elem)
+
+    pwd1_elem = '//input[@id="id_password1"]'
+    r.click(pwd1_elem)
+    r.type(pwd1_elem, new_password)
+    pwd2_elem = '//input[@id="id_password2"]'
+    r.click(pwd2_elem)
+    r.type(pwd2_elem, new_password)
+
+    submit_elem = '//input[@type="submit"]'
+    r.click(submit_elem)
+    print('CLICK')
+
+
 
 if __name__ == '__main__':
     env = 'staging'
+    user = 'drchepe'
+    pwd = '_N0SePuede0909'
+
     cred = get_credential_data(env)
     #print(cred)
     r.init()
@@ -33,6 +66,10 @@ if __name__ == '__main__':
     username = cred['roles']['admin']['username']
     password = cred['roles']['admin']['password']
     login_to_admin(admin_url, username, password)
+    ## CHANGE PASSWORD
+    r.wait(2)
+
+    change_password(user, pwd)
 
     r.wait(30)
     r.close()
